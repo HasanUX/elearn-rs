ada27dc030fbe67ff362c33c341d223b991dbb83
HasanUX <hrahatsharif@gmail.com>
2022-02-27 00:38:17 +0600

    gh-page added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,15 @@
 import React from "react";
+import HeroBanner from "../img/hero-banner-1.jpeg";
 import "./Hero.css";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="hero__banner">
+        <img src={HeroBanner} alt="hero-banner" className="hero__bannerImg" />
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
